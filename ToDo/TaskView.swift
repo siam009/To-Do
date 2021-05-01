e11a73e249ef4597ddf4703c16cbf588d5c8df36
Siam <arnab@bitmorpher.com>
2021-05-01 23:14:25 +0600

    view change added

---
+++ ToDo/TaskView.swift
@@ -0,0 +1,20 @@
+//
+//  TaskView.swift
+//  ToDo
+//
+//  Created by Bitmorpher 4 on 5/1/21.
+//
+
+import SwiftUI
+
+struct TaskView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TaskView_Previews: PreviewProvider {
+    static var previews: some View {
+        TaskView()
+    }
+}
